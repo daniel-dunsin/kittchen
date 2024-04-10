dba18acb4971cbcc2693b6b04cd42c710e16f5a2
daniel-dunsin <adejaredaniel12@gmail.com>
2024-04-10 15:03:07 +0100

    feat: setup

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        main: "linear-gradient(rgba(255, 122, 0, 1), rgba(248, 42, 42, 1))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
